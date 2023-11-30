f7d58777d12c02a684df3fa068ef270d0c45f711
Icedreck101 <nyamwezi.j@gmail.com>
2023-11-30 04:15:01 -0800

    #Task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
+    import math
     result = 0
     for i in sys.argv:
         result += int(i)
         print("{}".format(result))
-
